8707a9de607b26063e7cf1d0af7ad1c1e346ce28
Bruno Piccoli <brunoxp99@gmail.com>
2025-03-26 17:34:42 -0300

    Creating initial folder structure and testing first route.

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutineManager.API.Controllers
+{
+    [Route("api/teste")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Teste de rota" });
+        }
+    }
+}
